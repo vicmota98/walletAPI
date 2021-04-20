edbab5efec3a10d90df4e656de8e67e1f6469d32
vicmota98 <vicmota98@hotmail.com>
2021-04-20 15:18:29 -0300

    configuração inicial para os testes

--- src/test/java/com/victoria/wallet/HelloWorld.java
+++ src/test/java/com/victoria/wallet/HelloWorld.java
@@ -1,18 +1,18 @@
 package com.victoria.wallet;
 
-import org.junit.jupiter.api.Test;
+import org.junit.*;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles("test")
-class WalletApplicationTests {
+public class HelloWorld {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    public void testHelloWorld() {
 
+        assertEquals(1,1);
+    }
 }
